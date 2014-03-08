d1972620a7d274b04188ed9678d5cac70eff703f
jamoyer <jamoyer@iastate.edu>
2014-03-08 14:46:44 -0600

    Update browsing_test.rb
    Added comment for clarification.

--- test/performance/browsing_test.rb
+++ test/performance/browsing_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 require 'rails/performance_test_help'
 
+#This is another comment. Hopefully this counts as work.
 # Profiling results for each test method are written to tmp/performance.
 class BrowsingTest < ActionDispatch::PerformanceTest
   def test_homepage
